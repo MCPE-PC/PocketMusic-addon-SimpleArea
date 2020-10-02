867b0a1c3ca5d16ef51c3054d733361fdbc92693
MCPE_PC <maxpjh0528@naver.com>
2020-10-02 20:48:41 +0900

    Update IntervalPlaybackTask.php

--- addon/src/mcpepc/pocketmusic/addon/simplearea/IntervalPlaybackTask.php
+++ addon/src/mcpepc/pocketmusic/addon/simplearea/IntervalPlaybackTask.php
@@ -1,9 +1,31 @@
 <?php
+/**
+ * @author MCPE_PC
+ * @description Modified code of PlaySoundTask
+ * @refer mcpepc\pocketmusic\tasks\PlaySoundTask
+ */
 
 namespace mcpepc\pocketmusic\addon\simplearea;
 
-use pocketmine\event\Listener;
-use pocketmine\plugin\PluginBase;
+use mcpepc\pocketmusic\PocketMusic;
+use mcpepc\pocketmusic\Sound;
+use mcpepc\pocketmusic\tasks\PlaySoundTask;
+use mcpepc\pocketmusic\tasks\PocketMusicTask;
+use pocketmine\plugin\Plugin;
+use function strlen;
+use function strpos;
+use function substr;
 
-class IntervalPlaybackTask extends PluginBase implements Listener {
+class IntervalPlaybackTask extends PocketMusicTask {
+	private $args = [];
+
+	function __construct(Plugin $plugin, ...$args) {
+		$this->owningPlugin = $plugin;
+		$this->args = $args;
+	}
+
+	function onRun(int $currentTick) {
+		$this->getPlugin()->getScheduler()->scheduleTask(new PlaySoundTask($this->getPlugin()->getPocketMusic(), ...$args));
+		$this->getPlugin()->ready($player);
+	}
 }
