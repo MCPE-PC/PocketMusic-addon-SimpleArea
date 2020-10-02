a550312b51b95f812b1a7b40b14fe75d9dd57ab7
MCPE_PC <maxpjh0528@naver.com>
2020-10-02 20:30:51 +0900

    Create IntervalPlaybackTask.php

---
+++ addon/src/mcpepc/pocketmusic/addon/simplearea/IntervalPlaybackTask.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace mcpepc\pocketmusic\addon\simplearea;
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+class IntervalPlaybackTask extends PluginBase implements Listener {
+}
